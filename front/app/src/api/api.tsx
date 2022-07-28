7c7815a7e247321b6254858d989ae6835e538cd0
gustavogaldino <gcgm@cin.ufpe.br>
2022-07-28 20:06:04 -0300

    Added react-dotenv dependecy to manage env variables such as API_ENDPOINT

---
+++ front/app/src/api/api.tsx
@@ -0,0 +1,14 @@
+import env from 'react-dotenv'
+
+export default function api() {
+
+    function fetchParticipations () {
+        return;
+    }
+
+    function addParticipation () {
+        return;
+    }
+
+    return { fetchParticipations, addParticipation } 
+}
